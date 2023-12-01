96e60788887fda772c4c48691e8d1c329b5b1b48
Wesley Ireland <wireland@cvent.com>
2023-12-01 15:05:35 -0400

    2023 day 1

--- src/2023/day01.mjs
+++ src/2023/day01.mjs
@@ -1,4 +1,25 @@
-const fs = require('fs').promises;
+import * as fs from 'fs';
 
-const data = await fs.readFile('example.txt', 'utf8');  
-console.log(data);
+const data = await fs.promises.readFile('inputs/day01.txt', 'utf8');
+const lines = data.split(/\r?\n/);  
+
+function toDigit(str) {
+    const map = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
+    const digit = parseInt(str);
+    return isNaN(digit) ? map[str] : digit;
+}
+
+const part1 = lines.map(line => {
+    const first = line.match(/(\d)/)[1];
+    const last = line.match(/(\d)(?=\D*$)/)[1];
+    return parseInt(`${first}${last}`);
+}).reduce((acc, el) => acc + el, 0);
+
+const part2 = lines.map(line => {
+    const first = toDigit(line.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/)[1]);
+    const last = toDigit(line.match(/^.*(\d|one|two|three|four|five|six|seven|eight|nine).*$/)[1]);
+    return parseInt(`${first}${last}`);
+}).reduce((acc, el) => acc + el, 0);
+
+console.log(`Part 1: ${part1}`);
+console.log(`Part 2: ${part2}`);
