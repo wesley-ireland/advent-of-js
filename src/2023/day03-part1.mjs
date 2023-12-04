b1d1e8ddd8d966a83a066c2a13c6f8fd0035026f
Wesley Ireland <wesley.j.ireland@gmail.com>
2023-12-04 10:57:14 -0400

    2023 day 03 part 2

--- src/2023/day03-part1.mjs
+++ src/2023/day03-part1.mjs
@@ -40,25 +40,20 @@ function hasSymbolBelow(numberIndex, number, lineIndex, array) {
 }
 
 function hasAdjacentSymbol(line, number, lineIndex, array, numberIndex) {
-    const hasAdjacentSymbol = hasSymbolLeft(numberIndex, line) ||
+    return hasSymbolLeft(numberIndex, line) ||
         hasSymbolRight(numberIndex, number.toString().length, line) ||
         hasSymbolAbove(numberIndex, number, lineIndex, array) ||
         hasSymbolBelow(numberIndex, number, lineIndex, array);
-    console.log(`Number ${number} hasAdjacentSymbol ${hasAdjacentSymbol} line length ${line.length}, lineindex ${lineIndex}, numberIndex: ${numberIndex}`)
-    return hasAdjacentSymbol;
 }
 
 const part1 = lines.map((line, lineIndex, array) => {
-    let lineSum = 0;
-    const lineMatches = [...line.matchAll(/(\d+)/g)];
-    lineMatches.forEach(match => {
-        // console.log(line, match[1], lineIndex, array, match.index)
-       if (hasAdjacentSymbol(line, parseInt(match[1]), lineIndex, array, match.index)) {
-           lineSum += parseInt(match[1]);
-       }
-    });
-    return lineSum;
+    return [...line.matchAll(/(\d+)/g)].map(numberMatch => {
+        const number = parseInt(numberMatch[1]);
+        if (hasAdjacentSymbol(line, number, lineIndex, array, numberMatch.index)) {
+            return number;
+        };
+        return 0;
+    }).reduce((acc, el) => acc + el, 0);
 }).reduce((acc, el) => acc + el, 0);
 
 console.log(`Part 1: ${part1}`);
-// console.log(`Part 2: ${part2}`);
